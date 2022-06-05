833e3b0f662b0eb69e138cdd0b84f96c9b8c9e79
Fredo BASLER <megafredo@hotmail.com>
2022-06-05 09:00:07 +0200

    [feat] Function createUser ok

--- back/app/router/user.js
+++ back/app/router/user.js
@@ -5,10 +5,10 @@ import { fetchOneUser, createUser, updateUser, deleteUser, signInUser } from "..
 
 //* --------------------------- USER
 
-router.post('/users/profile', signInUser); // Connexion signIn
 router.get('/users/profile/:id', fetchOneUser); // Render Profil
 
-router.post('/users/profile', createUser); // Inscription signUp
+router.post('/users/signin', signInUser); // Connexion signIn
+router.post('/users/signup', createUser); // Inscription signUp
 
 router.patch('/users/profile/:id', updateUser); // Update
 router.delete('/users/profile/:id', deleteUser); // Delete
